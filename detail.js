1dccadbe6db0c00f35fcfd2b78747aae0a34cef2
Aman Rathod <as7677379564@gmail.com>
2021-08-28 18:14:20 +0530

    debounce search

---
+++ detail.js
@@ -0,0 +1,95 @@
+
+var container = document.getElementById('detail-container');
+
+const storedData = JSON.parse(window.sessionStorage.getItem('hero'));
+
+const superHeroId = window.location.search.split('=')[1];
+let data = [];
+
+for(let i = 0; i < storedData.length; ++i) {
+  if(storedData[i].id === superHeroId) {
+    data = storedData[i];
+    break;
+  }
+}
+
+console.log(data);
+
+const displayContent = () => {
+  container.innerHTML = '';
+  const content = `
+<div class="row-1">
+  <div>
+  <img src="${data.image.url}" /> 
+  </div>
+  <div class="profile-container">
+
+    <div><h1>${data.name}</h1></div>
+
+    <div>
+      <h2>Biography</h2>
+      <ul>
+      <li><strong>Full name :</strong> ${data.biography['full-name']}</li>
+      <li>Place of birth : ${data.biography['place-of-birth']}</li>
+      <li>First appearance : ${data.biography['first-appearance']}</li>
+      <li>Alignment : ${data.biography.alignment}</li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>Appearance</h2>
+      <ul>
+        <li><strong>Gender :</strong> ${data.appearance.gender}</li>
+        <li>Race: ${data.appearance.race}</li>
+        <li>Height : ${data.appearance.height[0]}</li>
+        <li>weight : ${data.appearance.weight[1]}</li>
+        <li>eye-color : ${data.appearance['eye-color']}</li>
+      </ul>
+    </div>
+    
+  </div>
+</div>
+
+<div class="row-2">
+
+  <div class="row-2-left">
+
+    <div>
+      <h2>PowerStats</h2>
+      <ul>
+        <li><strong>Intelligence :</strong> ${data.powerstats.intelligence}</li>
+        <li>Strength : ${data.powerstats.strength}</li>
+        <li>Speed : ${data.powerstats.speed}</li>
+        <li>Durability : ${data.powerstats.durability}</li>
+        <li>Power : ${data.powerstats.power}</li>
+      </ul>
+    </div>
+
+   
+    <div>
+      <h2>Connections</h2>
+      <ul>
+        <li><strong>Group affiliation :</strong> ${data.connections['group-affiliation']}</li>
+        <li>Relatives : ${data.connections.relatives}</li>
+      </ul>            
+    </div>
+
+  </div>
+    
+  <div class="row-2-right">
+    <div>
+      <h2>Work</h2>
+      <ul>
+        <li><strong>Occupation :</strong> ${data.work.occupation}</li>
+        <li>Base : ${data.work.base}</li>
+      </ul>
+    </div>
+  </div>
+
+  </div>
+    `;
+
+  container.innerHTML += content;
+}
+
+displayContent();
--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+
 var search_input = document.getElementById('search');
 var container = document.getElementById('container');
 
 const displayCards = (fetchData) => {
   container.innerHTML = '';
+  sessionStorage.clear();
+  sessionStorage.setItem('hero', JSON.stringify(fetchData));
   fetchData.forEach((item, idx) => {
-    console.log('item', item.name);
-    console.log('cont', container);
+    
+      console.log('calledd')
+      // localStorage.setItem('hero', JSON.stringify(item));
+    
+    // console.log('cont', container);
     const content=  `
     <div class="card-container">
       <div class="card-inner">
@@ -14,11 +20,12 @@ const displayCards = (fetchData) => {
           <h1>${item.name}</h1>
           <p>${item.connections.relatives}</p>
         </div>
-        <button class="btn">View</button>
+        <input type="button" class="btn" value="View" onclick="window.location.href='./details.html?id=${item.id}'">
+      </div>
       </div>
     `;
     container.innerHTML += content;
-
+   
   });
 }
 
@@ -35,8 +42,24 @@ const getSearchData = (superHero) => {
   });
 }
 
+const debounce = (func, wait) => {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
 let search_term = '';
 search_input.addEventListener('input', e => {
   search_term = e.target.value;
-  getSearchData(search_term);
+  if(search_term.length > 1) {
+    let word = search_term.split(' ');
+    for(let i = 0; i < word.length; i++) {
+        word[i] = word[i][0].toUpperCase() + word[i].substr(1);
+    }
+    // getSearchData(word.join(' '));
+    debounce(getSearchData(word.join(' ')), 250);
+  }
 })
